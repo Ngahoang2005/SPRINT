87a20cec5dd62265c7231ee5970fe5dccc156166
Ngahoang2005 <hoangthanhnga2005@gmail.com>
2026-07-02 11:32:13 +0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-IMAGES_DIR = './data/MUSTARD/testset/indic/hindi/'
+IMAGES_DIR = './data/MUSTARD/testset/indic/hindi/test/'
 DEVICE = '0'
 MODEL_PATH = './models/SPRINT_fintabnet.pt'
 BATCH_SIZE = 32
