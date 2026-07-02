2d4d4f28ab0d05c16790653b614eb08ad4c598a7
Ngahoang2005 <hoangthanhnga2005@gmail.com>
2026-07-02 11:56:24 +0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -2,4 +2,4 @@ IMAGES_DIR = './data/MUSTARD/testset/indic/hindi/test/'
 DEVICE = '0'
 MODEL_PATH = './models/SPRINT_fintabnet.pt'
 BATCH_SIZE = 32
-RESULTS_FILE = './../results.json'
+RESULTS_FILE = './results.json'
